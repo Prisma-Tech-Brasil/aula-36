b893ead9bef74ffedcf1cc94cddc8c53e972c35f
Wallace027dev <wallaceofc@hotmail.com>
2024-10-03 14:33:20 -0300

    bloco try catch

--- api/src/controllers/videosController.js
+++ api/src/controllers/videosController.js
@@ -2,63 +2,83 @@ const Video = require("../models/Video");
 const videosService = require("../services/videosService");
 
 class VideosController {
-  index(req, res) {
-    const filtros = req.query;
-    const videos = videosService.encontrarComFiltros(filtros);
+  index(req, res, next) {
+    try {
+      const filtros = req.query;
+      const videos = videosService.encontrarComFiltros(filtros);
   
-    if (videos.length > 0) {
-      return res.status(200).json(videos);
+      if (videos.length > 0) {
+        return res.status(200).json(videos);
+      }
+  
+      return res.status(404).json({ mensagem: "Nenhum vídeo encontrado" });
+    } catch (error) {
+      next(error); // Passa o erro para o middleware manipulador de erros
     }
-  
-    return res.status(404).json({ mensagem: "Nenhum vídeo encontrado" });
   }
 
   show(req, res, next) {
-    const video = videosService.buscarPeloId(req.id);
-    if (video) {
-      return res.status(200).json(video);
+    try {
+      const video = videosService.buscarPeloId(req.id);
+      if (video) {
+        return res.status(200).json(video);
+      }
+      return res.status(404).json({ mensagem: "Vídeo não encontrado" });
+    } catch (error) {
+      next(error); // Passa o erro para o middleware manipulador de erros
     }
-    return res.status(404).json({ mensagem: "Vídeo não encontrado" });
   }
 
   store(req, res, next) {
-    const { titulo, descricao, image, canalID } = req.body;
+    try {
+      const { titulo, descricao, image, canalID } = req.body;
 
-    if (!titulo || !descricao || !image || !canalID) {
-      return next(
-        new Error(
-          "Todos os campos (titulo, descricao, image, canalID) são obrigatórios."
-        )
-      );
+      if (!titulo || !descricao || !image || !canalID) {
+        return next(
+          new Error(
+            "Todos os campos (titulo, descricao, image, canalID) são obrigatórios."
+          )
+        );
+      }
+
+      const novoVideo = new Video(titulo, descricao, image, canalID);
+      videosService.adicionar(novoVideo);
+      return res.status(201).json(novoVideo);
+    } catch (error) {
+      next(error); // Passa o erro para o middleware manipulador de erros
     }
-
-    const novoVideo = new Video(titulo, descricao, image, canalID);
-    videosService.adicionar(novoVideo);
-    return res.status(201).json(novoVideo);
   }
 
   update(req, res, next) {
-    const video = videosService.buscarPeloId(req.id);
-    if (!video) {
-      return res.status(404).json({ mensagem: "Vídeo não encontrado" });
-    }
+    try {
+      const video = videosService.buscarPeloId(req.id);
+      if (!video) {
+        return res.status(404).json({ mensagem: "Vídeo não encontrado" });
+      }
 
-    videosService.atualizar(req.id, req.body);
-    return res
-      .status(200)
-      .json({ mensagem: "Vídeo atualizado com sucesso", video });
+      videosService.atualizar(req.id, req.body);
+      return res
+        .status(200)
+        .json({ mensagem: "Vídeo atualizado com sucesso", video });
+    } catch (error) {
+      next(error); // Passa o erro para o middleware manipulador de erros
+    }
   }
 
   delete(req, res, next) {
-    const video = videosService.buscarPeloId(req.id);
-    if (!video) {
-      return res.status(404).json({ mensagem: "Vídeo não encontrado" });
-    }
+    try {
+      const video = videosService.buscarPeloId(req.id);
+      if (!video) {
+        return res.status(404).json({ mensagem: "Vídeo não encontrado" });
+      }
 
-    videosService.excluir(req.id);
-    return res
-      .status(200)
-      .json({ mensagem: `Vídeo id:${req.id} removido com sucesso!`, video });
+      videosService.excluir(req.id);
+      return res
+        .status(200)
+        .json({ mensagem: `Vídeo id:${req.id} removido com sucesso!`, video });
+    } catch (error) {
+      next(error); // Passa o erro para o middleware manipulador de erros
+    }
   }
 }
 
