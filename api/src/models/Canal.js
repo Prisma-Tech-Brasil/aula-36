d3ce81cca4c21e3c356bbee47dd30e818e5c662e
Wallace027dev <wallaceofc@hotmail.com>
2024-10-04 13:29:53 -0300

    ajustes na model Canal

--- api/src/models/Canal.js
+++ api/src/models/Canal.js
@@ -1,16 +1,16 @@
 const { Usuario } = require("./Usuario");
 const Video = require("./Video");
 const { canais } = require("../mocks/dados.json");
+const gerarIdUnico = require("../utils/gerarIdUnico");
 
 class Canal extends Usuario {
-  constructor(id, nome, imagem, email, papel) {
-    super(nome, imagem, email, papel);
+  constructor(nome, imagem, email, papel, inscricoes) {
+    super(nome, imagem, email, papel, inscricoes);
     this.id = gerarIdUnico(canais);
     this.videos = [];
     this.inscritos = [];
   }
 
-  // Adicionar um novo vídeo
   static postarVideo(video) {
     const novoVideo = new Video(video);
     this.videos.push(novoVideo);
@@ -18,7 +18,6 @@ class Canal extends Usuario {
     return novoVideo;
   }
 
-  // Editar um vídeo por ID
   static editarVideo(idVideo, corpo) {
     const video = this.videos.find((video) => video.id === idVideo);
 
@@ -29,14 +28,12 @@ class Canal extends Usuario {
     return video;
   }
 
-  // Remover um vídeo por ID
   static excluirVideo(idVideo) {
     const index = this.videos.findIndex((video) => video.id === idVideo);
 
     return this.videos.splice(index, 1);
   }
 
-  //Gerenciar inscritos no canal
   static listarInscritos() {
     return this.inscritos;
   }
